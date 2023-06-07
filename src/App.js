8747f999d9201be44705730e3ea08b23bada641b
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2023-06-07 18:04:38 +0900

    Feat:"signup 페이지 생성"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Route, Routes as Switch } from 'react-router-dom';
 import './App.css';
+import Layout from './layout/Layout';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route element={<Layout />}>
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+           회원가입 페이지입니다.
+        </div>
+    );
+}
+
+export default SignUp;
