51e769ec158d3d6b261a68d047233dbe004614f7
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2023-06-08 11:48:20 +0900

    Feat:"mui 레이아웃"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import { Route, Routes as Switch } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
 import './App.css';
 import Layout from './layout/Layout';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#A48FFA',
+        main: '#6F4FF2',
+        dark: '#A48FFA',
+        contrastText: 'white',
+      },
+      secondary: {
+        light: '#A48FFA',
+        main: '#6F4FF2',
+        dark: '#A48FFA',
+        contrastText: '#000',
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <Switch>
-        <Route element={<Layout />}>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Route>
-      </Switch>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Switch>
+          <Route element={<Layout />}>
+            <Route path="/" redirect="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Route>
+        </Switch>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,11 +1,12 @@
+import { Box } from '@mui/material';
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
-        <div>
+        <Box sx={{ display: 'flex' }}>
             <Outlet />
-        </div>
+        </Box>
     );
 }
 
