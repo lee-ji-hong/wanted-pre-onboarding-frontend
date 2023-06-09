8e8eb4d062300610a08d5d45396c217e60bc38f2
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2023-06-09 16:44:16 +0900

    Feat: "토근 없을 때 로그인페이지로 이동"

--- src/pages/ToDoPage.js
+++ src/pages/ToDoPage.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import store from '../store/localStorage';
 import TodoList from '../components/Todo/ToDoList';
-
 const ToDoPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!store.getLocalStorage('item')) {
+      navigate(`/signin`);
+    }
+  }, []);
   return (
     <TodoList />
   );
