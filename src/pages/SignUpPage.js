6e9fb290c94d23a6150c0de0aecc66ea25137114
lee-ji-hong <88364280+lee-ji-hong@users.noreply.github.com>
2023-06-09 15:36:14 +0900

    Feat: "로컬스토리지에 토큰 있을 경우 /todo로 리다이렉트"

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import store from '../store/localStorage';
 import SignUp from '../components/SignUp/SignUp';
 
 const SignUpPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (store.getLocalStorage('item')) {
+      navigate(`/todo`);
+      console.log('로컬스토리지 있음')
+    }
+  }, []);
+
   return (
     <SignUp />
   )
